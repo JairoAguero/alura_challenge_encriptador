2aec99320ca9491327843919368e88bc8a683106
Jairo Aguero <99061817+JairoAguero@users.noreply.github.com>
2023-05-12 03:11:20 -0600

    update

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function msg() {
-  alert("Hello world!");
+const inpuTextArea = document.querySelector('.inPutText');
+const outpuTextArea = document.querySelector('.outPutText');
+function encriptar(stringEncriptada) {
+    let arrayCode = [["e", "enter"],["i", "imes"], ["a","ai"], ["o","ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    
+    for(let i = 0; i < arrayCode.length; i++) {
+        if(stringEncriptada.includes(arrayCode[i][0])) {
+        stringEncriptada = stringEncriptada.replaceAll(arrayCode[i][0], arrayCode[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inpuTextArea.value);
+    outpuTextArea.value = textoEncriptado;
+    outpuTextArea.style.backgroundImage = "none";
 }
