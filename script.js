c2c464acc1aff8f2b858b8d7f42dbb068478c51a
Jairo Aguero <99061817+JairoAguero@users.noreply.github.com>
2023-05-12 03:36:20 -0600

    update

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const inpuTextArea = document.querySelector('.inPutText');
 const outpuTextArea = document.querySelector('.outPutText');
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inpuTextArea.value);
+    outpuTextArea.value = textoEncriptado;
+    inpuTextArea.value = "";
+    outpuTextArea.style.backgroundImage = "none";
+}
+
 function encriptar(stringEncriptada) {
     let arrayCode = [["e", "enter"],["i", "imes"], ["a","ai"], ["o","ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
@@ -11,8 +19,21 @@ function encriptar(stringEncriptada) {
     }
     return stringEncriptada;
 }
-function btnEncriptar() {
-    const textoEncriptado = encriptar(inpuTextArea.value);
-    outpuTextArea.value = textoEncriptado;
-    outpuTextArea.style.backgroundImage = "none";
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(inpuTextArea.value);
+    outpuTextArea.value = textoDesencriptado;
+    inpuTextArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let arrayCode = [["e", "enter"],["i", "imes"], ["a","ai"], ["o","ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+    
+    for(let i = 0; i < arrayCode.length; i++) {
+        if(stringDesencriptada.includes(arrayCode[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(arrayCode[i][1], arrayCode[i][0]);
+        }
+    }
+    return stringDesencriptada;
 }
